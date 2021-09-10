afa6ba376bad4aacca3502105f53344c14de9fa8
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-09-11 00:01:30 +0100

    try debug

--- stage_harness.go
+++ stage_harness.go
@@ -1,5 +1,7 @@
 package tester_utils
 
+import "fmt"
+
 // StageHarness is passed to your Stage's TestFunc.
 //
 // If the program is a long-lived program that must be alive during the duration of the test (like a Redis server),
@@ -28,11 +30,16 @@ type StageHarness struct {
 }
 
 func (s StageHarness) RegisterTeardownFunc(teardownFunc func()) {
+	fmt.Println("registering teardown func")
 	s.teardownFuncs = append(s.teardownFuncs, teardownFunc)
 }
 
 func (s StageHarness) RunTeardownFuncs() {
+	fmt.Println("running teardown funcs...")
+	fmt.Println(len(s.teardownFuncs))
 	for _, teardownFunc := range s.teardownFuncs {
+		fmt.Println("running a teardown func")
 		teardownFunc()
+		fmt.Println("ran a teardown func")
 	}
 }
