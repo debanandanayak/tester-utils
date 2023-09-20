85bad60317c42ac7f408f531d3267ceb95328a9f
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-09-20 18:25:48 +0100

    Refactor TesterOutputTestCase struct and TestTesterOutput function

--- testing/tester_output_test_case.go
+++ testing/tester_output_test_case.go
@@ -1,6 +1,7 @@
 package testing
 
 import (
+	"encoding/json"
 	"fmt"
 	"path/filepath"
 	"testing"
@@ -17,8 +18,11 @@ type TesterOutputTestCase struct {
 	// ExpectedExitCode is the exit code that we expect the tester to return.
 	ExpectedExitCode int
 
-	// StageName is the name of the stage that we want to test.
-	StageName string
+	// UntilStageSlug is the slug of the stage that we want to test until. Either this or StageSlug must be provided.
+	UntilStageSlug string
+
+	// StageSlugs is the list of stages that we want to test. Either this or UntilStageSlug must be provided.
+	StageSlugs []string
 
 	// StdoutFixturePath is the path to the fixture file that contains the expected stdout output.
 	StdoutFixturePath string
@@ -26,7 +30,6 @@ type TesterOutputTestCase struct {
 	// NormalizeOutputFunc is a function that normalizes the tester's output. This is useful for removing things like timestamps.
 	NormalizeOutputFunc func([]byte) []byte
 }
-
 func TestTesterOutput(t *testing.T, testerDefinition tester_utils.TesterDefinition, testCases map[string]TesterOutputTestCase) {
 	m := stdio_mocker.NewStdIOMocker()
 	defer m.End()
@@ -35,7 +38,23 @@ func TestTesterOutput(t *testing.T, testerDefinition tester_utils.TesterDefiniti
 		t.Run(testName, func(t *testing.T) {
 			m.Start()
 
-			exitCode := runCLIStage(testerDefinition, testCase.StageName, testCase.CodePath)
+			if testCase.UntilStageSlug != "" && testCase.StageSlugs != nil && len(testCase.StageSlugs) > 0 {
+				t.Fatal("Either UntilStageSlug or StageSlugs must be provided, not both")
+			}
+
+			if testCase.UntilStageSlug == "" && (testCase.StageSlugs == nil || len(testCase.StageSlugs) == 0) {
+				t.Fatal("Either UntilStageSlug or StageSlugs must be provided")
+			}
+
+			var testCasesJson string
+
+			if testCase.UntilStageSlug != "" {
+				testCasesJson = buildTestCasesJsonUntilStageSlug(testCase.UntilStageSlug, testerDefinition)
+			} else {
+				testCasesJson = buildTestCasesJson(testCase.StageSlugs)
+			}
+
+			exitCode := runCLIStage(testerDefinition, testCasesJson, testCase.CodePath)
 			if !assert.Equal(t, testCase.ExpectedExitCode, exitCode) {
 				failWithMockerOutput(t, m)
 			}
@@ -46,12 +65,42 @@ func TestTesterOutput(t *testing.T, testerDefinition tester_utils.TesterDefiniti
 	}
 }
 
+func buildTestCasesJson(slugs []string) string {
+	testCases := []map[string]string{}
+
+	for index, slug := range slugs {
+		testCases = append(testCases, map[string]string{
+			"slug":            slug,
+			"test_log_prefix": fmt.Sprintf("test-%d", index+1),
+			"title":           fmt.Sprintf("Stage #%d: %s", index+1, slug),
+		})
+	}
+
+	testCasesJson, _ := json.Marshal(testCases)
+	return string(testCasesJson)
+}
+
+func buildTestCasesJsonUntilStageSlug(untilStageSlug string, testerDefinition tester_utils.TesterDefinition) string {
+	stageSlugs := []string{}
+
+	for _, testCase := range testerDefinition.TestCases {
+		stageSlugs = append(stageSlugs, testCase.Slug)
+
+		if testCase.Slug == untilStageSlug {
+			break
+		}
+	}
+
+	return buildTestCasesJson(stageSlugs)
+}
+
+
 //func normalizeTesterOutput(testerOutput []byte) []byte {
 //	re, _ := regexp.Compile("read tcp 127.0.0.1:\\d+->127.0.0.1:6379: read: connection reset by peer")
 //	return re.ReplaceAll(testerOutput, []byte("read tcp 127.0.0.1:xxxxx+->127.0.0.1:6379: read: connection reset by peer"))
 //}
 
-func runCLIStage(testerDefinition tester_utils.TesterDefinition, slug string, relativePath string) (exitCode int) {
+func runCLIStage(testerDefinition tester_utils.TesterDefinition, testCasesJson string, relativePath string) (exitCode int) {
 	// When a command is run with a different working directory, a relative path can cause problems.
 	path, err := filepath.Abs(relativePath)
 	if err != nil {
@@ -59,8 +108,8 @@ func runCLIStage(testerDefinition tester_utils.TesterDefinition, slug string, re
 	}
 
 	tester, err := tester_utils.NewTester(map[string]string{
-		"CODECRAFTERS_CURRENT_STAGE_SLUG": slug,
-		"CODECRAFTERS_SUBMISSION_DIR":     path,
+		"CODECRAFTERS_TEST_CASES_JSON": testCasesJson,
+		"CODECRAFTERS_SUBMISSION_DIR":  path,
 	}, testerDefinition)
 
 	if err != nil {
