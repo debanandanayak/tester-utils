eb3f21152a26bacdce9238ec3710905f67688bec
Ryan Gang <ryan-gg@outlook.com>
2024-05-27 16:40:55 +0530

    feat: colorize first differing bytes in bytes_diff_visualizer

--- bytes_diff_visualizer/bytes_diff_visualizer.go
+++ bytes_diff_visualizer/bytes_diff_visualizer.go
@@ -5,17 +5,47 @@ import (
 	"fmt"
 	"strings"
 	"text/tabwriter"
+
+	"github.com/fatih/color"
 )
 
 // VisualizeByteDiff visualizes the difference between two byte slices, returning lines to be presented to the user.
 //
 // The lines will include ANSI escape codes to colorize the output.
 func VisualizeByteDiff(actual []byte, expected []byte) []string {
+	byteDiffLines, row, col := visualizeByteDiff(actual, expected)
+
+	// Always odd, so get the size of each half exactly, +1 for the sepearator line
+	rowsInEachHalf := len(byteDiffLines)/2 + 1
+	// The line that contains the first differing byte, in the "expected" half
+	expectedLine := byteDiffLines[row]
+	// The line that contains the first differing byte, in the "actual" half
+	actualLine := byteDiffLines[row+rowsInEachHalf]
+	// The start and end index of the hex representation of the differing byte
+	// col is the index of the differing byte, so we have (col-1+1*2) bytes of hex representation before it, and (col*2) bytes of space
+	startIdxHex, endIdxHex := col*3, col*3+2
+	// We use the "|" separator to find the index of the differing byte in the ASCII representation
+	// We add 2 to the index to skip the space and the separator
+	idxAscii := strings.Index(expectedLine, "|") + 2 + col
+
+	byteDiffLines[row] = expectedLine[:startIdxHex] + colorize(color.FgHiGreen, expectedLine[startIdxHex:endIdxHex]) + expectedLine[endIdxHex:idxAscii] + colorize(color.FgHiGreen, expectedLine[idxAscii:idxAscii+1]) + expectedLine[idxAscii+1:]
+
+	byteDiffLines[row+rowsInEachHalf] = actualLine[:startIdxHex] + colorize(color.FgHiRed, actualLine[startIdxHex:endIdxHex]) + actualLine[endIdxHex:idxAscii] + colorize(color.FgHiRed, actualLine[idxAscii:idxAscii+1]) + actualLine[idxAscii+1:]
+
+	return byteDiffLines
+}
+
+// visualizeByteDiff visualizes the difference between two byte slices, returning lines to be presented to the user.
+// We also want to highlight the first differing byte in the output.
+// For that reason we return the row and column of the first differing byte.
+func visualizeByteDiff(actual []byte, expected []byte) ([]string, int, int) {
 	// If both are exactly the same, return an empty slice
 	if bytes.Equal(actual, expected) {
-		return []string{}
+		return []string{}, -1, -1
 	}
 
+	rows := 1 // "Expected ... line"
+	var firstDiffIndexRow, firstDiffIndexCol int
 	// Find the index of the first differing byte
 	var firstDiffIndex int = -1
 
@@ -47,6 +77,14 @@ func VisualizeByteDiff(actual []byte, expected []byte) []string {
 
 	for i := byteRangeStart; i < intmin(byteRangeEnd, len(expected)); i += byteCountPerLine {
 		end := intmin(i+byteCountPerLine, len(expected))
+
+		if firstDiffIndex >= i && firstDiffIndex < end {
+			firstDiffIndexCol = firstDiffIndex - i
+			firstDiffIndexRow = rows
+		} else {
+			rows += 1
+		}
+
 		fmt.Fprintf(tabWriter, "%v\t %v\n", formatBytesAsHex(expected[i:end]), formatBytesAsAscii(expected[i:end]))
 	}
 
@@ -64,7 +102,7 @@ func VisualizeByteDiff(actual []byte, expected []byte) []string {
 	if output[len(output)-1] == '\n' {
 		output = output[:len(output)-1]
 	}
-	return strings.Split(output, "\n")
+	return strings.Split(output, "\n"), firstDiffIndexRow, firstDiffIndexCol
 }
 
 func formatBytesAsAscii(value []byte) string {
@@ -104,4 +142,10 @@ func intmin(a int, b int) int {
 		return a
 	}
 	return b
+}
+
+func colorize(colorToUse color.Attribute, msg string) string {
+	colorizedLine := color.New(colorToUse).SprintFunc()(msg)
+
+	return colorizedLine
 }
