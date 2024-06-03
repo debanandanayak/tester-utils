df77beba503c640db93fe015173c6a2be59791ba
Ryan Gang <ryan-gg@outlook.com>
2024-06-03 20:18:47 +0530

    tests: update tests to include color

--- bytes_diff_visualizer/bytes_diff_visualizer_test.go
+++ bytes_diff_visualizer/bytes_diff_visualizer_test.go
@@ -1,17 +1,31 @@
 package bytes_diff_visualizer
 
 import (
+	"fmt"
+	"os"
+	"regexp"
 	"testing"
 
+	"github.com/fatih/color"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestMain(m *testing.M) {
+	fmt.Println("Setting up tests...")
+	// Enable color output for tests
+	// https://github.com/golang/go/issues/18153#issuecomment-264388969
+	// CL:https://go-review.googlesource.com/c/go/+/33857/2/doc/go1.8.html
+	color.NoColor = false
+
+	exitCode := m.Run()
+	os.Exit(exitCode)
+}
 
 func TestVisualizeByteDiffWorksWithStrings(t *testing.T) {
 	actual := []byte("Hello, World!")
 	expected := []byte("Hello, Go!")
 
 	result := VisualizeByteDiff(actual, expected)
-
 	if len(result) == 0 {
 		t.Errorf("Expected a non-empty result")
 	}
@@ -25,11 +39,12 @@ func TestVisualizeByteDiffWorksWithStrings(t *testing.T) {
 	}
 
 	for i, expectedLine := range expectedLines {
+		actualLine := stripANSI(result[i])
 		if i >= len(result) {
 			t.Fatalf("Expected %v lines, but only got %v", len(expectedLines), len(result))
 		}
 
-		if !assert.Equal(t, expectedLine, result[i]) {
+		if !assert.Equal(t, expectedLine, actualLine) {
 			t.FailNow()
 		}
 	}
@@ -52,11 +67,12 @@ func TestVisualizeByteDiffWorksWithNonPrintableCharacters(t *testing.T) {
 	}
 
 	for i, expectedLine := range expectedLines {
+		actualLine := stripANSI(result[i])
 		if i >= len(result) {
 			t.Fatalf("Expected %v lines, but only got %v", len(expectedLines), len(result))
 		}
 
-		if !assert.Equal(t, expectedLine, result[i]) {
+		if !assert.Equal(t, expectedLine, actualLine) {
 			t.FailNow()
 		}
 	}
@@ -85,14 +101,55 @@ func TestVisualizeByteDiffWorksWithLongerSequences(t *testing.T) {
 	}
 
 	for i, expectedLine := range expectedLines {
+		actualLine := stripANSI(result[i])
 		if i >= len(result) {
 			t.Fatalf("Expected %v lines, but only got %v", len(expectedLines), len(result))
 		}
 
-		if !assert.Equal(t, expectedLine, result[i]) {
+		if !assert.Equal(t, expectedLine, actualLine) {
 			t.FailNow()
 		}
 	}
 
 	assert.Equal(t, len(expectedLines), len(result))
+}
+
+func TestVisualizeByteDiffWorksWithColoredOutput(t *testing.T) {
+	actual := []byte("Hello, World!")
+	expected := []byte("Hello, Go!")
+
+	result := VisualizeByteDiff(actual, expected)
+	if len(result) == 0 {
+		t.Errorf("Expected a non-empty result")
+	}
+
+	expectedLines := []string{
+		"Expected (bytes 0-13), hexadecimal:                         | ASCII:",
+		"48 65 6c 6c 6f 2c 20 " + colorizeString(color.FgHiGreen, "47") + " 6f 21                               | Hello, " + colorizeString(color.FgHiGreen, "G") + "o!",
+		"",
+		"Actual (bytes 0-13), hexadecimal:                           | ASCII:",
+		"48 65 6c 6c 6f 2c 20 " + colorizeString(color.FgHiRed, "57") + " 6f 72 6c 64 21                      | Hello, " + colorizeString(color.FgHiRed, "W") + "orld!",
+	}
+
+	for i, expectedLine := range expectedLines {
+		actualLine := result[i]
+		if i >= len(result) {
+			t.Fatalf("Expected %v lines, but only got %v", len(expectedLines), len(result))
+		}
+
+		if !assert.Equal(t, expectedLine, actualLine) {
+			t.FailNow()
+		}
+	}
+
+	assert.Equal(t, len(expectedLines), len(result))
+}
+
+func stripANSI(data string) string {
+	// https://github.com/acarl005/stripansi/blob/master/stripansi.go
+	const ansi = "[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))"
+
+	var re = regexp.MustCompile(ansi)
+
+	return string(re.ReplaceAll([]byte(data), []byte("")))
 }
