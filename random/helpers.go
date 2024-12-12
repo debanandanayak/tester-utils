e2ef4b4879a3651bc2ad08ef954a5f6b64df12ea
Ryan Gang <ryan-gg@outlook.com>
2024-12-12 12:35:44 +0530

    Add RandomInts function to generate unique random integers

--- random/helpers.go
+++ random/helpers.go
@@ -3,6 +3,7 @@ package random
 import (
 	"math/rand"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 	"time"
@@ -37,9 +38,28 @@ func Init() {
 	}
 }
 
-// RandomInt returns a random integer between min and max.
+// RandomInt returns a random integer between [min, max)
 func RandomInt(min, max int) int {
 	return rand.Intn(max-min) + min
+}
+
+// RandomInts returns a list of n unique random integers between [min, max)
+func RandomInts(min, max int, count int) []int {
+	randomInts := []int{}
+
+	if count > max-min {
+		panic("can't generate more unique random integers than the range of possible values")
+	}
+
+	for i := 0; i < count; i++ {
+		randomInt := RandomInt(min, max)
+		for slices.Contains(randomInts, randomInt) {
+			randomInt = RandomInt(min, max)
+		}
+		randomInts = append(randomInts, randomInt)
+	}
+
+	return randomInts
 }
 
 // RandomWord returns a random word from the list of words.
